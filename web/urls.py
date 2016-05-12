a52ac4d9ce33f2b1b1c75ad1614a41dcb7445b10
brendan sudol <brendansudol@gmail.com>
2016-05-12 00:13:06 -0400

    add project detail page stub

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from web.views.home import HomeView
+from web.views.project import ProjectView
 
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^p/(?P<pk>[\w/\-]+)?$', ProjectView.as_view(), name='project'),
 ]
