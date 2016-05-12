3f7b8cc15f1fee88102da76c71010f0b0cf9eeca
Atul Varma <varmaa@gmail.com>
2016-05-12 07:19:37 -0400

    Added some MVP fields to Project model.

--- projects/models.py
+++ projects/models.py
@@ -10,5 +10,33 @@ class ModelBase(models.Model):
 
 
 class Project(ModelBase):
-    name = models.CharField(max_length=256)
-    description = models.TextField()
+    name = models.CharField(
+        max_length=100,
+        help_text='The full name of the project (e.g., "Agile BPA")'
+    )
+    slug = models.CharField(
+        max_length=100,
+        help_text='The slug of the project (e.g., "agile-bpa")'
+    )
+    tock_id = models.IntegerField(
+        help_text='The ID of the project in Tock.'
+    )
+    tagline = models.CharField(
+        max_length=300,
+        help_text='The tagline of the project; short and concise.'
+    )
+    impact = models.TextField(
+        help_text='The impact of the project. Markdown is allowed.'
+    )
+    github_url = models.URLField(
+        help_text='The GitHub URL of the project, e.g. '
+                  'https://github.com/18f/agile-bpa'
+    )
+    description = models.TextField(
+        help_text='The description of the project. Markdown is allowed.'
+    )
+    active = models.BooleanField(
+        help_text='Whether or not the project is currently being '
+                  'worked on at 18F.',
+        default=True
+    )
