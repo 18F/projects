7b0cd9c9f59bbc0c694fc94cac7232b9977e3e00
Brendan Sudol <brendan.sudol@gsa.gov>
2016-05-20 10:24:15 -0400

    add columns, filters, search to admin

--- projects/admin.py
+++ projects/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from .models import Client, Project
 
 
-admin.site.register(Client)
-admin.site.register(Project)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('department', 'agency',)
+    list_filter = ('department',)
+    search_fields = ('department', 'agency',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'client',)
+    list_filter = ('status', 'billable', 'cloud_dot_gov',)
+    search_fields = ('name',)
