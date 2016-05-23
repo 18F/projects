3568870b0fb05407c6fb936cb22ce920ea843732
Brendan Sudol <brendan.sudol@gsa.gov>
2016-05-23 16:40:24 -0400

    add basic sorting

--- web/views/home.py
+++ web/views/home.py
@@ -6,16 +6,22 @@ from projects.models import Project
 class HomeView(ListView):
     context_object_name = 'projects'
     model = Project
-    ordering = '-created'
+    ordering = 'name'
     paginate_by = 100
     template_name = 'web/index.html'
+    sort_fields = ['name', 'created', '-created']
 
     def get_queryset(self):
         qs = super(HomeView, self).get_queryset()
+        get = self.request.GET
 
-        q = self.request.GET.get('q')
+        q = get.get('q')
         if q:
             qs = self.model.objects.search(q)
+
+        s = get.get('s')
+        if s and s in self.sort_fields:
+            qs = qs.order_by(s)
 
         return qs
 
